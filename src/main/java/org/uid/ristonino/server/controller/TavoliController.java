636c2552a6037dc78aa325f2eeea5c0b8711fef9
GhadPRG <ghadprogrammer@gmail.com>
2024-05-12 16:17:17 +0200

    Creata idea design Dashboard Responsive. Abbozzato scheletro tavoli con scenebuilder.

--- src/main/java/org/uid/ristonino/server/controller/TavoliController.java
+++ src/main/java/org/uid/ristonino/server/controller/TavoliController.java
@@ -1,4 +1,48 @@
+//Idea sidebar gestore tavoli: se la sidebar(anchor pane) è visibile, cambiare dimensioni parte centrale
+//Oppure, impostare alla sidebar una grandezza=0 se non è visibile, e quando sarà visibile impostare una grandezza
+//del 15% della larghezza della scena centrale
 package org.uid.ristonino.server.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.FlowPane;
+import org.kordamp.ikonli.javafx.FontIcon;
+
 public class TavoliController {
+
+    @FXML
+    private FlowPane flowPaneTavoli;
+
+    @FXML
+    private AnchorPane sideBarTavoli;
+
+    @FXML
+    private Button tavoloButton;
+
+    @FXML
+    private Label tavoloLabel;
+
+
+    //Fare lista di bottoni e inserirla sul flowpane
+    public void initialize() {
+        FontIcon myIcon = new FontIcon("mdi2p-plus-thick"); //MaterialDesign2
+        myIcon.setIconSize(20);
+        tavoloLabel.setGraphic(myIcon);
+        tavoloLabel.setText("");
+        FontIcon tableIcon = new FontIcon("mdi2t-table-furniture");
+        tableIcon.setIconSize(45);
+        tavoloButton.setGraphic(tableIcon);
+        tavoloButton.setText("");
+    }
+
+
+    @FXML
+    void addTable(MouseEvent event) {
+        System.out.println("Sono qua");
+
+    }
+
 }
--- src/main/java/org/uid/ristonino/server/view/SceneHandler.java
+++ src/main/java/org/uid/ristonino/server/view/SceneHandler.java
@@ -116,5 +116,6 @@ public class SceneHandler {
     public Stage getStage() {
         return stage;
     }
+
 }
 
