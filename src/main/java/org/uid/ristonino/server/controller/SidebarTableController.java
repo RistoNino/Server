6b55f8394c588bf1036a2cbbec044b6c1b0b3545
GhadPRG <ghadprogrammer@gmail.com>
2024-05-29 21:32:52 +0200

    Come dicevo prima, il codice che gestiva i tavoli è stato stravolto. Infatti, ora per la gestione della sidebar c'è questa classe. Esegue le cose simili al vecchio controller dei tavoli.

---
+++ src/main/java/org/uid/ristonino/server/controller/SidebarTableController.java
@@ -0,0 +1,97 @@
+//TODO: Verificare che i pulsanti siano effettivamente collegati anche se si rimuove un tavolo
+//TODO: Aggiungere tasto per lo scontrino
+
+package org.uid.ristonino.server.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.util.Pair;
+import org.uid.ristonino.server.model.services.OrderService;
+import org.uid.ristonino.server.model.services.TableService;
+import org.uid.ristonino.server.model.types.Item;
+import org.uid.ristonino.server.view.SceneHandler;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class SidebarTableController {
+    @FXML
+    public Label tableText;
+    @FXML
+    public Label totalText;
+    @FXML
+    public VBox orderVBox;
+
+    OrderService orderService = OrderService.getInstance();
+    TableService tableService = TableService.getInstance();
+
+    private static SidebarTableController instance;
+
+    @FXML
+    private AnchorPane sidebarRight;
+
+    public SidebarTableController() {
+        instance = this; // Imposta l'istanza corrente come singleton
+    }
+
+    public static SidebarTableController getInstance() {
+        return instance;
+    }
+
+    @FXML
+    public void initialize() {
+        closeSidebar();
+        orderService.update();
+        tableService.update();
+
+    }
+
+    public void openSidebar(int numTav) throws IOException {
+        orderVBox.getChildren().clear();
+        sidebarRight.setPrefWidth((SceneHandler.getInstance().sideWidht(20)));
+        if (sidebarRight != null) {
+            tableText.setText("Ordine Tavolo "+numTav+":");
+            ArrayList<Pair<Integer, Item>> ord =orderService.getOrder(numTav);
+            for(int i=0; i<ord.size();i++)
+            {
+                FXMLLoader loader= new FXMLLoader(getClass().getResource("/org/uid/ristonino/server/view/templete/item.fxml"));
+                Node item=loader.load();
+                ItemTableController controller = loader.getController();
+
+                int quantity=ord.get(i).getKey();
+                String nomePiatto=ord.get(i).getValue().getName();
+                String notePiatto=ord.get(i).getValue().getNote();
+                double prezzoPiatto=ord.get(i).getValue().getPrice();
+
+                orderVBox.getChildren().add(controller.setItem(nomePiatto, notePiatto, prezzoPiatto, quantity));
+            }
+            int nCoperti=tableService.getCoperti(numTav);
+            Label coperti =new Label();
+            double prezzoCoperti=1.50;
+            coperti.setText(nCoperti+"x coperti €"+prezzoCoperti);
+            coperti.setStyle("-fx-font-size: 13; -fx-font-weight: 700");
+            orderVBox.getChildren().add(coperti);
+
+
+
+            totalText.setText("Totale: €"+(orderService.getTotal(numTav)+((double)nCoperti*prezzoCoperti)));
+            sidebarRight.setVisible(true);
+
+
+        }
+        else{
+            System.out.println("sidebarRight is null");
+        }
+    }
+
+    public void closeSidebar(){
+        tableText.setText("");
+        totalText.setText("");
+        sidebarRight.setPrefWidth(0);
+        sidebarRight.setVisible(false);
+    }
+}
