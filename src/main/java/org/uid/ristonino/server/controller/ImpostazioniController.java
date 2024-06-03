25f7c689c2d4da916963325aa16734bf5df893d3
GhadPRG <ghadprogrammer@gmail.com>
2024-06-03 16:22:21 +0200

    Creato controller per tasto impostazioni

---
+++ src/main/java/org/uid/ristonino/server/controller/ImpostazioniController.java
@@ -0,0 +1,8 @@
+//TODO: Possibilità di modificare il logo con un immagine a scelta
+//TODO: Possibilità cambiare tema (se aggiungiamo i temi)
+
+
+package org.uid.ristonino.server.controller;
+
+public class ImpostazioniController {
+}
